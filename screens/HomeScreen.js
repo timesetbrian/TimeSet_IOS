87bc4173f9c4c77aec9edd978400d3370cc72b8f
briandjin <Brian@timeset.com>
2017-07-21 16:02:20 -0400

    Renders list of carousel

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TabNavigator } from 'react-navigation';
-import ImageCarousel from '../components/ImageCarousel';
+import ImageCarouselList from '../components/ImageCarouselList';
 
 export default class HomeScreeen extends Component {
   
   render() {
     return (
-      <ImageCarousel />
+      <ImageCarouselList />
     );
   }
 }
